55c5ef668d7c0c36eb7d9780d7fb2cee11e0bffe
Kasia <kays.katarzyna@gmail.com>
2024-06-25 10:59:40 -0700

    api filters

--- frontend/src/actions/filters.jsx
+++ frontend/src/actions/filters.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/parks/api/filters";
+const API_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001";
 
 export function fetchFilterDataFromAPI() {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`${API_URL}`);
+      const response = await axios.get(`${API_URL}/parks/api/filters`);
       return dispatch(getFilters(response.data));
     } catch (e) {
       console.log(e);
