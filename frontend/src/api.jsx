bb9f334ff305fd2e86e277c031fc6e85398f3d8f
Kasia <kays.katarzyna@gmail.com>
2024-06-25 10:36:16 -0700

    api

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = "https://pin-the-park.onrender.com" || "http://localhost:3001";
 
 class ParksApi {
   static token;
